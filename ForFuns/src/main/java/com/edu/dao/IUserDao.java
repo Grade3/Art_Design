4d491eca76a5191fe8e8f920d135f66ff5065637
Christy <511955993@qq.com>
2015-11-21 21:52:11 +0800

    gy-Init

---
+++ ForFuns/src/main/java/com/edu/dao/IUserDao.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.edu.dao;
+
+import com.edu.base.IBaseDao;
+import com.edu.model.UserBean;
+
+
+public interface IUserDao extends IBaseDao<UserBean>{
+	
+	public boolean isExist(UserBean user);
+}
